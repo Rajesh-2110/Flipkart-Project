a737fd04b579764901d787e97a4a9f262ee4d748
Rajesh R <rajeshrckr2110@gmail.com>
2025-07-28 03:52:52 -0700

    Updated LeftSidebar and RightSidebar

--- src/LeftSidebar.js
+++ src/LeftSidebar.js
@@ -1,19 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LeftSidebar.css";
 
 function LeftSidebar() {
+  const [brand, setBrand] = useState("");
+  const [price, setPrice] = useState("");
+  const [offer, setOffer] = useState("");
+
+  const handleInputBrand = (e) => {
+    setBrand(e.target.value);
+  };
+  const handleInputPrice = (e) => {
+    setPrice(e.target.value);
+  };
+  const handleInputOffer = (e) => {
+    setOffer(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(brand);
+    console.log(price);
+    console.log(offer);
+  };
+
   return (
     <div className="sidebar-left">
       <div className="filter-section">
         <h3>Size - UK/India</h3>
         {[6, 7, 8, 9, 10].map((size) => (
-          <label>
-            <input type="checkbox" /> {size}
+          <label key={size}>
+            <input type="checkbox" />
+            {size}
           </label>
         ))}
+      </div>
+      <div className="form-left">
+        <form action="post" onSubmit={handleClick}>
+          <legend>Product Details</legend>
+          <input
+            type="text"
+            name="brandName"
+            className="input-brand"
+            placeholder="Brand Name"
+            value={brand}
+            onChange={handleInputBrand}
+            required
+          />
+          <input
+            type="number"
+            name="price"
+            className="input-number"
+            placeholder="Enter Price"
+            value={price}
+            onChange={handleInputPrice}
+            required
+          />
+          <input
+            type="url"
+            name="imageUrl"
+            placeholder="Enter Image URL"
+            className="input-url"
+          />
+          <input
+            type="text"
+            name="offer"
+            className="input-offer"
+            value={offer}
+            onChange={handleInputOffer}
+            placeholder="Offer %"
+            required
+          />
+          <input type="submit" className="submit" value="Submit" />
+        </form>
       </div>
     </div>
   );
 }
 
-export default LeftSidebar;   
+export default LeftSidebar;
--- src/RightSidebar.js
+++ src/RightSidebar.js
@@ -1,82 +1,75 @@
 import "./RightSidebar.css";
-import whiteShoes from './white-shoes.jpg';
-import phone from './Phone.jpg'
-import sunglasses from './polarized sunglasses.jpg';
-import rackets from './rackets.jpg';
-import shoes from './shuttle shoes.jpg';
-import watch from './Titan-watches.jpg';
+import whiteShoes from "./white-shoes.jpg";
+import phone from "./Phone.jpg";
+import sunglasses from "./polarized sunglasses.jpg";
+import rackets from "./rackets.jpg";
+import shoes from "./shuttle shoes.jpg";
+import watch from "./Titan-watches.jpg";
 
-function RightSidebar(){
-    const shoeprice = 991;
-    const phprice = 70000;
-    const sunglass = 1101;
-    const yonex = 1550;
-    const shuttle_shoes = 510;
-    const titan_watch = 4200;
-    return(
-        <div className='card-container'>
-        <div className="card">
-            <img src={whiteShoes} alt="white-shoe" className="product-img" />
-            <p className="brand-name">Sketchers</p>
-            <p className="price">
-              {shoeprice.toLocaleString('en-IN', { style: 'currency', currency: 'INR' })}
-            </p>
-            <p className="discount">51% off</p>
-            <span className="hot-deal">Hot Deal</span>
-        </div>
-       
-        <div className="card">
-            <img src={phone} alt="iphone" className='product-img' />
-            <p className="brand-name">IPhone 13 Pro</p>
-            <p className="price">
-              {phprice.toLocaleString('en-IN', { style: 'currency', currency: 'INR' })}
-            </p>
-            <p className="discount">30% off</p>
-            <span className="hot-deal">Hot Deal</span>
-        </div>
+function RightSidebar() {
+  const products = [
+    {
+      id: 1,
+      image: whiteShoes,
+      name: "Sketchers",
+      price: 991,
+      discount: "51%",
+    },
+    {
+      id: 2,
+      image: phone,
+      name: "IPhone 13pro",
+      price: 79999,
+      discount: "30%",
+    },
+    {
+      id: 3,
+      image: sunglasses,
+      name: "Polarized Sunglass",
+      price: 1150,
+      discount: "25%",
+    },
+    {
+      id: 4,
+      image: rackets,
+      name: "Yonex Nanoray Light",
+      price: 1998,
+      discount: "31%",
+    },
+    {
+      id: 5,
+      image: shoes,
+      name: "Yonex shuttle shoe",
+      price: 510,
+      discount: "50%",
+    },
+    {
+      id: 6,
+      image: watch,
+      name: "Titan",
+      price: 4500,
+      discount: "15%",
+    },
+  ];
 
-        <div className="card">
-            <img src={sunglasses} alt="sunglass" className='product-img' />
-            <p className="brand-name">Polarized sunglass</p>
-            <p className="price">
-              {sunglass.toLocaleString('en-IN', { style: 'currency', currency: 'INR' })}
-            </p>
-            <p className="discount">20% off</p>
-            <span className="hot-deal">Hot Deal</span>
+  return (
+    <div className="card-container">
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt={product.name} className="product-img" />
+          <p className="brand-name">{product.name}</p>
+          <p className="price">
+            {product.price.toLocaleString("en-IN", {
+              style: "currency",
+              currency: "INR",
+            })}
+          </p>
+          <p className="discount">{product.discount} off</p>
+          <span className="hot-deal">hot-deal</span>
         </div>
-
-        <div className="card">
-            <img src={rackets} alt="shuttle-rackets" className='product-img' />
-            <p className="brand-name">Yonex</p>
-            <p className="price">
-              {yonex.toLocaleString('en-IN',{style: 'currency', currency: 'INR'})}
-            </p>
-            <p className="discount">11% off</p>
-            <span className="hot-deal">Hot Deal</span>
-        </div>
- 
-        <div className="card">
-            <img src={shoes} alt="shuttle-shoes" className='product-img' />
-            <p className="brand-name">Victor</p>
-            <p className="price">
-              {shuttle_shoes.toLocaleString('en-IN',{style:'currency',currency: 'INR'})}
-            </p>
-            <p className="discount">50% off</p>
-            <span className="hot-deal">Hot Deal</span>
-        </div>
-
-        <div className="card">
-            <img src={watch} alt="titan-watch" className='product-img' />
-            <p className="brand-name">Titan</p>
-            <p className="price">
-              {titan_watch.toLocaleString('en-IN',{style:'currency',currency: 'INR'})}
-            </p>
-            <p className="discount">50% off</p>
-            <span className="hot-deal">Hot Deal</span>
-        </div>
-          
-        </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default RightSidebar;
